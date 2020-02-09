472c878a6dffe18ee75d671610e6f0a6588f783e
Glitch (jsx-starter) <none>
2020-02-09 02:14:31 +0000

    🐦👱 Updated with Glitch

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 const domContainer = document.querySelector('#app-root');
 
+const {Fragment} = React;
 const getRect = h => (<rect key={`Rect:${h}`} x="0" y={h} width="150" height="1" style={{
       fill: `hsl(${h},100%,50%)`
     }} />);
 const rectList = [...new Array(360)].map((v, i) => getRect(i));
-const segDist = 360 / 15;
-const segmentList = [...new Array(15)].map((v, i) => (
-  <rect
+const nameList = [
+  'red',
+  'orange',
+  'yellow',
+  'yellow green',
+  'green',
+  'mint',
+  'green aqua',
+  'aqua',
+  'aqua blue',
+  'blue',
+  'blue purple',
+  'purple',
+  'pink',
+  'pink red',
+  'red'
+];
+const segDist = 360 / nameList.length;
+const segmentList = nameList.map((v, i) => (
+  <Fragment>
+    <rect
     x="0"
     y={i * segDist}
     width="150"
@@ -17,12 +36,17 @@ const segmentList = [...new Array(15)].map((v, i) => (
         fill: 'none'
     }}
   />
+    <text x="10" y={(i * segDist) + 15} style={{fill: 'white'}}>{v}</text>
+  </Fragment>
 ));
 
 ReactDOM.render(
-  <svg viewBox="0 0 150 360" width="150" height="360">
-    {rectList}
-    {segmentList}
-  </svg>, 
+  <Fragment>
+    Number Of Colors: {nameList.length}<br/>
+    <svg viewBox="0 0 150 360" width="150" height="360" style={{fontFamily: 'sans-serif'}}>
+      {rectList}
+      {segmentList}
+    </svg>
+  </Fragment>, 
   domContainer
 );
